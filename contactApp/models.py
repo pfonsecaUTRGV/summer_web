4fd1fd1f6a89dfb208a663ef23a26d3d5528038d
PFonseca <pedro.fonseca01@utrgv.edu>
2025-08-13 10:26:40 -0500

    add model and home view

--- contactApp/models.py
+++ contactApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+class Record(models.Model): 
+	create_at = models.DateTimeField(auto_now_add=True)
+	first_name = models.CharField(max_length=20)
+	last_name = models.CharField(max_length=20)
+	#email = models.EmailField(max_length=30)
+	email = models.CharField(max_length=30)
+	phone = models.CharField(max_length=10)
+
+	def __str__(self):
+		return(f"{self.first_name}{self.last_name}")
--- contactApp/views.py
+++ contactApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Record
+
 
 
 def home(request): 
-	return render(request, 'home.html',{})
+	records = Record.objects.all()
+	#return render(request, 'home.html',{})
+	return render(request, 'home.html', {'records': records})
